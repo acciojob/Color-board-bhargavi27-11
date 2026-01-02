80428029e2cd0c8e9f610ee29b1c2066474945e3
sesetty bhargavi <sesettybhargavi@gmail.com>
2026-01-02 23:04:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ const SQUARES = 800;
 
 const colors = ['#e74c3c','#8e44ad','#3498db','#e67e22','#2ecc71'];
 
-for(let i = 0; i<SQUARES ; i++){
+for(let i = 0; i < SQUARES ; i++){
 	const square = document.createElement('div');
 	square.classList.add('square');
 
-	square.addEventListener('mouseover',() => setColor(square));
-	square.addEventListener('mouseout',() => removeColor(square));
+	square.addEventListener('mouseover', () => setColor(square));
+	square.addEventListener('mouseout', () => removeColor(square));
 
 	container.appendChild(square);
 }
@@ -21,7 +21,9 @@ function setColor(element) {
 
 function removeColor(element) {
 	//const color = getRandomColor();
-	element.style.backgroundColor = "#1d1d1d";
+	setTimeout(() => {
+		element.style.backgroundColor = "rgb(29,29,29)";
+	}, 1000);
 }
 
 function getRandomColor() {
